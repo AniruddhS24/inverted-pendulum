47f03a266e2d8476ec7097abafa2db297994aeb6
aniruddhsriram <aniruddh.sriram@gmail.com>
2023-12-09 18:43:17 -0600

    latest changes

--- data.py
+++ data.py
@@ -10,10 +10,24 @@ class Environment:
                                 render_mode="human")
         else:
             self.env = gym.make("InvertedDoublePendulum-v4")
+        # self.env = gym.wrappers.RecordEpisodeStatistics(self.env)
+        self.env = gym.wrappers.ClipAction(self.env)
+        self.env = gym.wrappers.NormalizeObservation(self.env)
+        self.env = gym.wrappers.TransformObservation(
+            self.env, lambda obs: np.clip(obs, -10, 10))
+        self.env = gym.wrappers.NormalizeReward(self.env)
+        self.env = gym.wrappers.TransformReward(
+            self.env, lambda reward: np.clip(reward, -10, 10))
+
+    def observation_space(self):
+        return self.env.observation_space.shape
+
+    def action_space(self):
+        return self.env.action_space.shape
 
     def reset(self, seed):
-        observation, info = self.env.reset(seed=seed)
-        return observation, info
+        observation, _ = self.env.reset(seed=seed)
+        return observation
 
     def step(self, action):
         observation, reward, terminated, truncated, info = self.env.step(
@@ -34,7 +48,7 @@ class Environment:
 
     def sample_episode(self, policy, max_steps=500):
         trajectory = []
-        observation, info = self.reset(seed=42)
+        observation = self.reset(seed=42)
         trajectory.append({'timestep': 0,
                            'state': observation,
                            'action': 0,
@@ -52,6 +66,12 @@ class Environment:
             if terminated or truncated:
                 return trajectory
         return trajectory
+
+
+def make_env():
+    def thunk():
+        return Environment(render=False).env
+    return thunk
 
 
 def random_policy(observation):
